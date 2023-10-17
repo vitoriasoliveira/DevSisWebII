32d010043bdb87fb62c3cb43c964cc21e8dedfc9
vitoriasoliveira <77559436+vitoriasoliveira@users.noreply.github.com>
2023-10-17 11:59:43 -0300

    Create IntroHTML.php

---
+++ Aula1/IntroHTML.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR"> <!-- identify which language you want your site to be  -->
+
+<head>
+    <title>Introdução HTML</title>
+    <meta charset="UFT-8"> <!-- defines how to deal with characters, not mandatory, but a good practice -->
+    <meta name="viewport" name="width=device-width,initial-scale=1.0"><!-- site is responsive, adapts to each navigator, not mandatory, but a good practice -->
+</head>
+
+<body>
+    <?php
+    $name = "Vitória";
+    $date = date("m/d/y");
+    print "Hello $name, today is $date";
+    ?>
+</body>
+
+</html>
